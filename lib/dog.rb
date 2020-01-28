cdcc6bc82d67a84ef5b4dba0c6713fca63723a30
Chris Wade <swade781977@yahoo.com>
2020-01-28 20:37:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,13 @@
 class Dog
-  def nameinit(dog_name)
-    this_dogs_name = dog_name
+  def name = (dog_name)
+    @this_dogs_name = dog_name
   end
   def name
-    puts this_dogs_name
+    @this_dogs_name
   end
 end
 
 lassie = Dog.new 
-lassie.nameinit("Lassie")
-lassie.name
+lassie.name = "Lassie"
+puts lassie.name
 
