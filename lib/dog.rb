e0e7f4bde687e293820a47a4e2369d2cf916ea63
Chris Wade <swade781977@yahoo.com>
2020-01-28 20:34:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,7 @@ class Dog
     this_dogs_name = dog_name
   end
   def name
-    this_dogs_name
+    puts this_dogs_name
   end
 end
 
@@ -11,4 +11,3 @@ lassie = Dog.new
 lassie.nameinit("Lassie")
 lassie.name
 
-puts lassie.name
